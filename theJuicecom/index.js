c07df15fb6a95a80abb2068a9aa075bd641bb05e
gustavolopesnobrega <gustavo.lopes.nobrega@gmail.com>
2023-03-01 14:32:18 -0300

    first commit

---
+++ theJuicecom/index.js
@@ -0,0 +1,11 @@
+let unidadeJp = document.getElementById("unidadejp").value 
+let unidadeUk = document.getElementById("unidadelondres").value
+let unidadeHavai = document.getElementById("unidadehavai").value  
+console.log(unidadeJp)
+console.log(unidadeUk)
+console.log(unidadeHavai)
+
+
+
+let selectUnidade = document.getElementById("unidade")
+console.log(selectUnidade)
